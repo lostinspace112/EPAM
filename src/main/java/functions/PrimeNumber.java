2011f24ece2debfd8199a61124239c6d4612759e
Plesa Dan <plesadandumitru@gmail.com>
2019-07-29 20:17:18 +0200

    Added Prime Number and Second Longest Caracter

---
+++ src/main/java/functions/PrimeNumber.java
@@ -0,0 +1,29 @@
+package functions;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = s.nextInt();
+        if (isPrime(n)) {
+            System.out.println(n + "True");
+        } else {
+            System.out.println(n + "False");
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
---
+++ src/main/java/functions/SecondLongestCaracterChain.java
@@ -0,0 +1,42 @@
+package functions;
+
+public class SecondLongestCaracterChain {
+    static final int NO_OF_CHARS = 256;
+
+
+    static char getSecondMostFreq(String str)
+    {
+        int[] count = new int[NO_OF_CHARS];
+        int i;
+        for (i=0; i< str.length(); i++)
+            (count[str.charAt(i)])++;
+
+        int first = 0, second = 0;
+        for (i = 0; i < NO_OF_CHARS; i++)
+        {
+            if (count[i] > count[first])
+            {
+                second = first;
+                first = i;
+            }
+
+            else if (count[i] > count[second] &&
+                    count[i] != count[first])
+                second = i;
+        }
+
+        return (char)second;
+    }
+
+    public static void main(String args[])
+    {
+        String str = "aabbbcwwww";
+        char res = getSecondMostFreq(str);
+        if (res != '\0')
+            System.out.println(res);
+        else
+            System.out.println("No second most frequent"+
+                    "character");
+    }
+}
+
